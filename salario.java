4d0ea153375a943d31eda4369dc08301a1ef4c28
Gledson Lima <58941795+gledlima@users.noreply.github.com>
2021-11-25 22:12:32 -0300

    Add files via upload

---
+++ salario.java
@@ -0,0 +1,13 @@
+public class salario {
+    
+    int cartaodeponto;
+    int salarioMensal;
+    int salarioComissão;
+
+    public assalariado(){
+        r
+    }
+    public horistas(){
+        cartaodeponto = cartaodeponto + 
+    }
+}
